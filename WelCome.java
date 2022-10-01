d3635bd5cceb65344bdf9a2ca5377c1b03063321
kavyahallur123 <kavyahallur199@gmail.com>
2022-10-01 13:37:38 +0530

    line comparision

---
+++ WelCome.java
@@ -0,0 +1,7 @@
+package com.bridgelab.line_comparision;
+
+public class WelCome {
+    public static void main(String[] args) {
+        System.out.println("Wel-come to Line Comparision codes");
+    }
+}
